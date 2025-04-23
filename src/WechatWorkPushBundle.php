59ba0fe64d18bd16d52105bc87b03b3c5b3739e0
github-ci <you@example.com>
2025-04-23 07:19:04 +0000

    refactor(wechat-work-push-bundle): 移除无用的接口实现和注解

--- src/WechatWorkPushBundle.php
+++ src/WechatWorkPushBundle.php
@@ -3,17 +3,7 @@
 namespace WechatWorkPushBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '企业微信推送')]
-class WechatWorkPushBundle extends Bundle implements BundleDependencyInterface
+class WechatWorkPushBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \WechatWorkBundle\WechatWorkBundle::class => ['all' => true],
-            \WechatWorkMediaBundle\WechatWorkMediaBundle::class => ['all' => true],
-        ];
-    }
 }
